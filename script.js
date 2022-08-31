d0bb6fa3bf30b935e9979b8ea95fbf3e989053d8
JonathanPlanells <jonathanplanells@hotmail.com>
2022-08-31 18:22:39 -0500

    desarrollo funcion js

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let select = document.getElementById('ciudad');
-select.addEventListener('change', function sel(event){
+select.addEventListener('change', function sel(event) {
     console.log(event.target.value)
 })
 
-
-
-function funcionBuscar(){
-    if (document.getElementById('ciudad').value == "BARRANQUILLA"){
-        document.location.href = "destinos.html",true;
-    }else{
+function funcionBuscar() {
+    if (document.getElementById('ciudad').value == "BARRANQUILLA") {
+        document.location.href = "destinos.html", true;
+    } else if (document.getElementById('ciudad').value == "BARICHARA") {
+        document.location.href = "destinos.html", true;
+    } else if (document.getElementById('ciudad').value == "BOGOTA") {
+        document.location.href = "destinos.html", true;
+    } else if (document.getElementById('ciudad').value == "CALI") {
+        document.location.href = "destinos.html", true;
+    } else if (document.getElementById('ciudad').value == "CARTAGENA") {
+        document.location.href = "destinos.html", true;
+    } else if (document.getElementById('ciudad').value == "SAN ANDRES") {
+        document.location.href = "destinos.html", true;
+    } else if (document.getElementById('ciudad').value == "SANTA MARTA") {
+        document.location.href = "destinos.html", true;
+    } else if (document.getElementById('ciudad').value == "LETICIA") {
+        document.location.href = "destinos.html", true;
+    } else {
         console.log("error")
     }
 };
+
+

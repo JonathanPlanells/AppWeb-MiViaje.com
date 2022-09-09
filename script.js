27cd881292c8d27df728a12a2e53ddaf0b5d62e0
JonathanPlanells <jonathanplanells@hotmail.com>
2022-09-09 18:07:21 -0500

    Creación de Modales para formulario de busqueda index

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Función para obtener el valor del select de ciudad
 let select = document.getElementById('ciudad');
 select.addEventListener('change', function sel(event) {
     console.log(event.target.value)
 })
 
+// Condicional para formulario de busqueda
 function funcionBuscar() {
+     // Validando que las fechas del checkOut no sea antes del checkIN
     if (document.getElementById('fechaIN').value > document.getElementById('fechaOut').value) {
-        alert("La fecha de checkout no puede ser antes que la de check in");
+        mostrarModalFechas()
     } else {
         if (document.getElementById('ciudad').value == "BARRANQUILLA") {
             document.location.href = "destinos.html", true;
@@ -24,34 +27,75 @@ function funcionBuscar() {
         } else if (document.getElementById('ciudad').value == "LETICIA") {
             document.location.href = "destinos.html", true;
         } else {
-            showModal()
+            mostrarModalDestino() // Modal seleccionar Destino
         }
-        passInformacion()
+        passInformacion() //Ejecutamos la funcion de guardar información para luego utilizarla en los otros html
     }
 };
 
+// Funcion para guardar y pasar información de un html a otro
 function passInformacion() {
     let fechaIN = document.getElementById('fechaIN').value;
-    localStorage.setItem("fechaCheckIn", fechaIN);
+    localStorage.setItem("fechaCheckIn", fechaIN); // Guardando en el localStorage la fecha del checkIn
     let fechaOUT = document.getElementById('fechaOut').value;
-    localStorage.setItem("fechaCheckOut", fechaOUT);
+    localStorage.setItem("fechaCheckOut", fechaOUT); // Guardando en el localStorage la fecha del checkOut
     let destinoSelec = document.getElementById('ciudad').value;
-    localStorage.setItem("ciudadSeleccion", destinoSelec);
+    localStorage.setItem("ciudadSeleccion", destinoSelec); // Guardando en el localStorage la ciudad
     return true;
 }
 
-//function verificarFecha(){
-
+// Funcion para establcer la fecha del Check In y no permitir escoger una fecha anterior
+function verificarFechaIN() {
+    let fecha = new Date(); //Fecha actual
+    let mes = fecha.getMonth() + 1; //obteniendo mes
+    let dia = fecha.getDate(); //obteniendo dia
+    let ano = fecha.getFullYear(); //obteniendo año
+    if (dia < 10)
+        dia = '0' + dia; //agrega cero si el menor de 10
+    if (mes < 10)
+        mes = '0' + mes //agrega cero si el menor de 10
+    let fechaHoy = ano + "-" + mes + "-" + dia;
+    document.getElementById('fechaIN').min = ano + "-" + mes + "-" + dia;
+    document.getElementById('fechaOut').min = ano + "-" + mes + "-" + dia;
+    document.getElementById('fechaIN').value = fechaHoy;
+    document.getElementById('fechaOut').value = fechaHoy;
+}
+// Funcion para limpiar el localStorage
 function clearStorage() {
     localStorage.clear();
 }
 
-function showModal() {
+// Funcion para iniciar funciones que necesitemos al iniciar la pagina
+function ejecutarAlCargarPagina(){
+    verificarFechaIN();
+    clearStorage();
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// FUNCIONES PARA MODALES 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function mostrarModalDestino() {
     document.getElementById('openModal').style.display = 'block';
-  }
+}
 
-function CloseModal() {
+function mostrarModalFechas() {
+    document.getElementById('openModal2').style.display = 'block';
+}
+
+function CerrarModal() {
     document.getElementById('openModal').style.display = 'none';
-  }
+}
 
+function CerrarModal2() {
+    document.getElementById('openModal2').style.display = 'none';
+}
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// LLAMADA DE FUNCION
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+window.onload = ejecutarAlCargarPagina;
 CloseModal()
+CerrarModal2()
